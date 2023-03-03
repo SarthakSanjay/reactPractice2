2cae7a20d67a0db803cef071e81563ef580caf77
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-03-04 01:26:13 +0530

    ic

--- rpractice/src/App.jsx
+++ rpractice/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import BookList from './components/Booklist'
 import BookListMap from './components/BookListMap'
 
 function App() {
--- rpractice/src/components/BookListMap.jsx
+++ rpractice/src/components/BookListMap.jsx
@@ -1,44 +1,81 @@
 import React from 'react'
 
-const BookListMap = () => {
-  return (
-    <div>
-        {booklist.map((book)=>{
-            const {name , author , image} = book
-            return <Book name={name} image={image} author={author} />
-        })}
-
-    </div>
-  )
-}
-
 const booklist = [
     {
-        name:"harry potter",
-        author:"jk rowling",
+        id: 1,
+        name: "harry potter",
+        author: "jk rowling",
         image: "https://m.media-amazon.com/images/I/5165He67NEL.jpg"
     },
     {
-        name:"Marvel Comics",
-        author:"stan lee",
+        id: 2,
+        name: "Marvel Comics",
+        author: "stan lee",
         image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR8vAcAYEGsZvyWLdkkW9Gemhxamm1Ip1RNxM5NP5NCZZFh4GRapA80u4KajwkEYR7dzpMcl0Ck24e1_xUiPULD1jrZdhDkDujMGpGfKoE"
     },
     {
-        name:"DC comics",
-        author:"warner bros",
+        id: 3,
+        name: "DC comics",
+        author: "warner bros",
         image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRLtBapSgavJgGxTgMgC-Q-9DRivYiyOw9EXeFeX7iXiVvYytGz"
     },
 ]
 
-const Book = ({name , author , image }) => { 
-    return(
+// const friend = ['sharko', 'shubham','akshay']
+// const allFriend = [...friend , 'babe rexha']
+// console.log(allFriend)
+const BookListMap = () => {
+    return (
         <div>
-        <img src={image} alt="" style={{width:"200px" , height:"300px"}} />
+            {/* {booklist.map((book) => {
+            const {name , author , image , id} = book
+            return <Book name={name} image={image} author={author} key={id} />
+            // key is always provided in the main return always remember that
+        })} */}
+
+            
+               { // if we want to send whole object as prop we can do it to
+                /* booklist.map((book) => {
+                    const { name, author, image, id } = book
+                    return <Book book={book} key={id} />
+                    // key is always provided in the main return always remember that
+                }) */}
+            
+            {
+                booklist.map((book)=>{
+                    return <Book {...book} key={book.id} />
+                })
+            }
+        </div>
+    )
+}
+
+
+
+//const Book = (props) => { // since prop is an object and inside it we have another object  book above is the syntax to destructure it
+   // console.log(props)
+    //this will output
+    //{book: {…}}
+   
+//     const Book = ({book: {image , name , author}}) => {
+// return (
+//     <div >
+//         <img src={image} alt="" style={{ width: "200px", height: "300px" }} />
+//         <h1>{name}</h1>
+//         <h3>{author}</h3>
+//     </div>
+// )
+// }
+const Book = (props) => {
+    const { name, author, image } = props
+    return (
+        <div >
+            <img src={image} alt="" style={{ width: "200px", height: "300px" }} />
             <h1>{name}</h1>
             <h3>{author}</h3>
         </div>
     )
-}
+    }
 
 
 export default BookListMap
